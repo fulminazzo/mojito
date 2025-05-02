3cb35ce17705b67d4ba47e0488d8207a8b1934b2
user <fulminazzo21@gmail.com>
2025-05-02 23:44:30 +0200

    Added tests for invalid update on constant variable

--- src/test/groovy/it/fulminazzo/mojito/environment/ScopeTest.groovy
+++ src/test/groovy/it/fulminazzo/mojito/environment/ScopeTest.groovy
@@ -10,6 +10,24 @@ class ScopeTest extends Specification {
         this.scope = new Scope<>(ScopeType.CODE_BLOCK)
     }
 
+    def 'test update on final variable'() {
+        given:
+        def varName = NamedEntity.of('var')
+
+        and:
+        this.scope.declare(new WrapperInfo<>(String), varName, '1')
+
+        and:
+        this.scope.markConstant(varName)
+
+        when:
+        this.scope.update(varName, '2')
+
+        then:
+        def e = thrown(ScopeException)
+        e.message == ScopeException.cannotUpdateConstantVariable(varName).message
+    }
+
     def 'test lookupInfo not declared'() {
         given:
         def varName = 'var'
