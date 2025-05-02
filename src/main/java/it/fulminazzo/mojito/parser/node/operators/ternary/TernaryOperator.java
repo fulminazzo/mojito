b6ac339d3d741ec36f316aac863cf209db3aaf7d
user <fulminazzo21@gmail.com>
2025-05-02 22:25:22 +0200

    Created TernaryOperator class

---
+++ src/main/java/it/fulminazzo/mojito/parser/node/operators/ternary/TernaryOperator.java
@@ -0,0 +1,22 @@
+package it.fulminazzo.mojito.parser.node.operators.ternary;
+
+import it.fulminazzo.mojito.parser.node.Node;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Represents the Java ternary operator operation.
+ */
+public class TernaryOperator extends TernaryOperation {
+
+    /**
+     * Instantiates a new Ternary operator.
+     *
+     * @param expression   the expression
+     * @param firstResult  the first result
+     * @param secondResult the second result
+     */
+    public TernaryOperator(@NotNull Node expression, @NotNull Node firstResult, @NotNull Node secondResult) {
+        super(expression, firstResult, secondResult);
+    }
+
+}
