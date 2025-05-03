bc46bc0c0e394540699a0e37ba0ef80527fbfed2
user <fulminazzo21@gmail.com>
2025-05-03 20:06:44 +0200

    Added tests for equals method

--- src/test/groovy/it/fulminazzo/mojito/typechecker/types/objects/GenericsObjectClassTypeTest.groovy
+++ src/test/groovy/it/fulminazzo/mojito/typechecker/types/objects/GenericsObjectClassTypeTest.groovy
@@ -6,6 +6,47 @@ import spock.lang.Specification
 
 class GenericsObjectClassTypeTest extends Specification {
 
+    def 'test equals method'() {
+        given:
+        def first = new GenericsObjectClassType(
+                ObjectType.of('List'),
+                [ObjectClassType.STRING]
+        )
+
+        and:
+        def second = new GenericsObjectClassType(
+                ObjectType.of('List'),
+                [ObjectClassType.STRING]
+        )
+
+        expect:
+        first == second
+    }
+
+    def 'test #second should not equal'() {
+        given:
+        def first = new GenericsObjectClassType(
+                ObjectType.of('List'),
+                [ObjectClassType.STRING]
+        )
+
+        expect:
+        first != second
+
+        where:
+        second << [
+                new GenericsObjectClassType(
+                        ObjectType.of('List'),
+                        [ObjectClassType.INTEGER]
+                ),
+                new GenericsObjectClassType(
+                        ObjectType.of('Map'),
+                        [ObjectClassType.STRING, ObjectClassType.INTEGER]
+                ),
+                ObjectClassType.of('List')
+        ]
+    }
+
     def 'test toString method'() {
         given:
         def type = ObjectType.of(List)
