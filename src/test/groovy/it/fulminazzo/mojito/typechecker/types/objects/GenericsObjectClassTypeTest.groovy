0ce3ecea8eeb34917753a3bc2ccc09dade3575b3
user <fulminazzo21@gmail.com>
2025-05-03 19:13:04 +0200

    Added tests for invalid size

--- src/test/groovy/it/fulminazzo/mojito/typechecker/types/objects/GenericsObjectClassTypeTest.groovy
+++ src/test/groovy/it/fulminazzo/mojito/typechecker/types/objects/GenericsObjectClassTypeTest.groovy
@@ -1,6 +1,7 @@
 package it.fulminazzo.mojito.typechecker.types.objects
 
-
+import it.fulminazzo.mojito.typechecker.TypeCheckerException
+import it.fulminazzo.mojito.typechecker.types.ClassType
 import spock.lang.Specification
 
 class GenericsObjectClassTypeTest extends Specification {
@@ -17,6 +18,27 @@ class GenericsObjectClassTypeTest extends Specification {
 
         then:
         classType.genericTypes['E'] == genericTypes[0]
+    }
+
+    def 'test that initialization with #parameter throws'() {
+        given:
+        def type = ObjectType.of(List)
+
+        and:
+        def genericTypes = (1..parameter).collect { ObjectClassType.STRING }
+        if (parameter == 0) genericTypes.clear()
+
+        when:
+        new GenericsObjectClassType(type, genericTypes)
+
+        then:
+        def e = thrown(TypeCheckerException)
+        e.message == TypeCheckerException.invalidGenericTypeSize(
+                ClassType.of(List), 1, parameter
+        ).message
+
+        where:
+        parameter << [0, 2, 3]
     }
 
 }
