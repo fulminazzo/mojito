a1085adf67216433a7dae41739e6dd45a905fdd5
user <fulminazzo21@gmail.com>
2025-05-03 19:57:58 +0200

    Added tests for toString

--- src/test/groovy/it/fulminazzo/mojito/typechecker/types/objects/GenericsObjectClassTypeTest.groovy
+++ src/test/groovy/it/fulminazzo/mojito/typechecker/types/objects/GenericsObjectClassTypeTest.groovy
@@ -6,6 +6,23 @@ import spock.lang.Specification
 
 class GenericsObjectClassTypeTest extends Specification {
 
+    def 'test toString method'() {
+        given:
+        def type = ObjectType.of(List)
+
+        and:
+        def genericTypes = [ObjectClassType.STRING]
+
+        when:
+        def classType = new GenericsObjectClassType(type, genericTypes)
+
+        and:
+        def output = classType.toString()
+
+        then:
+        output == 'List<String.class>.class'
+    }
+
     def 'test that initialization with correct parameters does not throw'() {
         given:
         def type = ObjectType.of(List)
