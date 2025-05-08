6d9b856a0c6b9cbf6c271ba2b0d86f6db0908b13
user <fulminazzo21@gmail.com>
2025-05-08 16:47:39 +0200

    Implemented equals, hashCode and toString methods

--- src/main/java/it/fulminazzo/mojito/typechecker/types/objects/generics/GenericsObjectType.java
+++ src/main/java/it/fulminazzo/mojito/typechecker/types/objects/generics/GenericsObjectType.java
@@ -1,18 +1,18 @@
-package it.fulminazzo.mojito.typechecker.types.objects;
+package it.fulminazzo.mojito.typechecker.types.objects.generics;
 
 import it.fulminazzo.mojito.typechecker.TypeCheckerException;
 import it.fulminazzo.mojito.typechecker.types.ClassType;
 import it.fulminazzo.mojito.typechecker.types.ParameterTypes;
 import it.fulminazzo.mojito.typechecker.types.Type;
 import it.fulminazzo.mojito.typechecker.types.TypeException;
+import it.fulminazzo.mojito.typechecker.types.objects.ObjectType;
 import it.fulminazzo.mojito.visitors.visitorobjects.ClassVisitorObject;
 import it.fulminazzo.mojito.wrappers.ObjectWrapper;
 import org.jetbrains.annotations.NotNull;
 
 import java.lang.reflect.TypeVariable;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class GenericsObjectType extends ObjectWrapper<Class<?>> implements Type {
     private final Map<String, ClassType> genericTypes;
@@ -20,7 +20,7 @@ public class GenericsObjectType extends ObjectWrapper<Class<?>> implements Type 
     public GenericsObjectType(final @NotNull Class<?> clazz,
                               final @NotNull List<ClassType> genericTypes) {
         super(clazz);
-        this.genericTypes = new HashMap<>();
+        this.genericTypes = new LinkedHashMap<>();
         TypeVariable<? extends Class<?>>[] typeParameters = clazz.getTypeParameters();
 
         if (typeParameters.length != genericTypes.size())
@@ -42,6 +42,7 @@ public class GenericsObjectType extends ObjectWrapper<Class<?>> implements Type 
 
     @Override
     public @NotNull ClassType toClass() {
+        //TODO:
         return null;
     }
 
@@ -58,6 +59,29 @@ public class GenericsObjectType extends ObjectWrapper<Class<?>> implements Type 
     @Override
     public boolean isAssignableFrom(@NotNull ClassVisitorObject<ClassType, Type, ParameterTypes> classVisitorObject) {
         return Type.super.isAssignableFrom(classVisitorObject);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getClass().hashCode(), this.object, this.genericTypes);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof GenericsObjectType) {
+            GenericsObjectType other = (GenericsObjectType) o;
+            return this.object.equals(other.object) && this.genericTypes.equals(other.genericTypes);
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return Type.print(ObjectType.getClassName(this.object) + String.format("<%s>",
+                this.genericTypes.values().stream()
+                        .map(Object::toString)
+                        .collect(Collectors.joining(", "))
+        ));
     }
 
 }
