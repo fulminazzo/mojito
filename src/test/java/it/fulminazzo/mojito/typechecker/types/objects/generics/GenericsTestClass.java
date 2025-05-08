050475b1e4f205b2c00b8870261a02297f4c4c81
user <fulminazzo21@gmail.com>
2025-05-08 18:01:22 +0200

    Added field

--- src/test/java/it/fulminazzo/mojito/typechecker/types/objects/generics/GenericsTestClass.java
+++ src/test/java/it/fulminazzo/mojito/typechecker/types/objects/generics/GenericsTestClass.java
@@ -1,7 +1,14 @@
 package it.fulminazzo.mojito.typechecker.types.objects.generics;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Comparator;
 
+@Getter
+@Setter
 class GenericsTestClass<F extends Number & Comparator<String>> {
+
+    private F first;
 
 }
