843c76acbdc7ac6537a4123e33b8918842b996aa
user <fulminazzo21@gmail.com>
2025-05-08 16:53:21 +0200

    Removed unused tests

--- src/test/groovy/it/fulminazzo/mojito/typechecker/types/objects/GenericsObjectClassTypeTest.groovy
+++ src/test/groovy/it/fulminazzo/mojito/typechecker/types/objects/GenericsObjectClassTypeTest.groovy
@@ -1,7 +1,5 @@
 package it.fulminazzo.mojito.typechecker.types.objects
 
-import it.fulminazzo.mojito.typechecker.TypeCheckerException
-import it.fulminazzo.mojito.typechecker.types.ClassType
 import spock.lang.Specification
 
 class GenericsObjectClassTypeTest extends Specification {
@@ -62,65 +60,6 @@ class GenericsObjectClassTypeTest extends Specification {
 
         then:
         output == 'List<String.class>.class'
-    }
-
-    def 'test that initialization with correct parameters does not throw'() {
-        given:
-        def type = ObjectType.of(List)
-
-        and:
-        def genericTypes = [ObjectClassType.STRING]
-
-        when:
-        def classType = new GenericsObjectClassType(type, genericTypes)
-
-        then:
-        classType.genericTypes['E'] == genericTypes[0]
-    }
-
-    def 'test that initialization with #parameter throws'() {
-        given:
-        def type = ObjectType.of(List)
-
-        and:
-        def genericTypes = (1..parameter).collect { ObjectClassType.STRING }
-        if (parameter == 0) genericTypes.clear()
-
-        when:
-        new GenericsObjectClassType(type, genericTypes)
-
-        then:
-        def e = thrown(TypeCheckerException)
-        e.message == TypeCheckerException.invalidGenericTypeSize(
-                ClassType.of(List), 1, parameter
-        ).message
-
-        where:
-        parameter << [0, 2, 3]
-    }
-
-    def 'test that exception is thrown when parameter with invalid bounds is passed'() {
-        given:
-        def type = ObjectType.of(TestClass)
-
-        when:
-        new GenericsObjectClassType(type, [parameter])
-
-        then:
-        def e = thrown(TypeCheckerException)
-        e.message == TypeCheckerException.invalidType(
-                bound, parameter
-        ).message
-
-        where:
-        parameter                | bound
-        ClassType.of(String)     | ClassType.of(Number)
-        ClassType.of(Number)     | new GenericsObjectClassType(ObjectType.of(Comparator), [ObjectClassType.STRING])
-        ClassType.of(Comparator) | ClassType.of(Number)
-    }
-
-    static class TestClass<F extends Number & Comparator<String>> {
-
     }
 
 }
