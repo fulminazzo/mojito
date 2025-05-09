2ba7389bcffb8aa2a605030617236407cfe29f34
user <fulminazzo21@gmail.com>
2025-05-09 14:00:24 +0200

    Updated compatibleWith method

--- src/main/java/it/fulminazzo/mojito/typechecker/types/objects/generics/GenericsObjectClassType.java
+++ src/main/java/it/fulminazzo/mojito/typechecker/types/objects/generics/GenericsObjectClassType.java
@@ -58,18 +58,9 @@ public class GenericsObjectClassType extends CustomObjectClassType implements Cl
 
     @Override
     public boolean compatibleWith(@NotNull Type type) {
-        if (type.is(GenericsObjectType.class)) {
-            GenericsObjectType genericsObjectType = (GenericsObjectType) type;
-            if (!toJavaClass().isAssignableFrom(genericsObjectType.getInnerClass()))
-                return false;
-            Map<String, ClassType> genericTypes = genericsObjectType.getGenericTypes();
-            for (String key : this.genericTypes.keySet()) {
-                if (!genericTypes.containsKey(key)) return false;
-                if (!this.genericTypes.get(key).is(genericTypes.get(key)))
-                    return false;
-            }
-            return true;
-        } else return super.compatibleWith(type);
+        if (type.is(GenericsObjectType.class))
+            return GenericsUtils.checkCompatibility(this, (GenericsObjectType) type);
+        else return super.compatibleWith(type);
     }
 
     @Override
