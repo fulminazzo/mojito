ef550993af0a7d8c2bbebff5651be5410e206ccc
user <fulminazzo21@gmail.com>
2025-05-09 17:03:45 +0200

    Updated MethodContainerImpl

--- src/main/java/it/fulminazzo/mojito/visitors/visitorobjects/executables/MethodContainerImpl.java
+++ src/main/java/it/fulminazzo/mojito/visitors/visitorobjects/executables/MethodContainerImpl.java
@@ -7,7 +7,7 @@ import java.lang.reflect.Method;
 /**
  * An implementation of {@link ExecutableContainer} for {@link Method}s.
  */
-class MethodContainerImpl extends ExecutableContainerImpl implements ExecutableContainer {
+class MethodContainerImpl extends ExecutableContainerImpl<Method> {
 
     /**
      * Instantiates a new Method container.
