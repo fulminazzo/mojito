22213ca8eb367e04e0639966a2734cda3e70b971
user <fulminazzo21@gmail.com>
2025-05-10 21:20:08 +0200

    Added tests for toLiteral method

--- src/test/groovy/it/fulminazzo/mojito/parser/node/literals/GenericsLiteralTest.groovy
+++ src/test/groovy/it/fulminazzo/mojito/parser/node/literals/GenericsLiteralTest.groovy
@@ -4,6 +4,31 @@ import spock.lang.Specification
 
 class GenericsLiteralTest extends Specification {
 
+    def 'test toLiteral'() {
+        given:
+        def literal = new GenericsLiteral('List', [
+                new LiteralImpl('String')
+        ])
+
+        when:
+        def newLiteral = literal.toLiteral()
+
+        then:
+        newLiteral == new LiteralImpl('List')
+    }
+
+    def 'test toLiteral exception for JaCoCo coverage'() {
+        given:
+        def literal = Spy(GenericsLiteral, constructorArgs: ['List', []])
+        literal.getLiteral() >> '!?.'
+
+        when:
+        literal.toLiteral()
+
+        then:
+        thrown(IllegalStateException)
+    }
+
     def 'test that toString prints internal types'() {
         given:
         def literal = new GenericsLiteral('List', [
