95754aee7566d10eca7957eb9328447e863941e8
user <fulminazzo21@gmail.com>
2025-05-10 22:00:03 +0200

    Added tests of list with methods and fields

--- src/test/groovy/it/fulminazzo/mojito/FullTests.groovy
+++ src/test/groovy/it/fulminazzo/mojito/FullTests.groovy
@@ -1,5 +1,6 @@
 package it.fulminazzo.mojito
 
+import it.fulminazzo.mojito.executor.values.Value
 import it.fulminazzo.mojito.typechecker.TypeCheckerException
 import it.fulminazzo.mojito.typechecker.types.ClassType
 import it.fulminazzo.mojito.typechecker.types.objects.ObjectClassType
@@ -10,6 +11,26 @@ import spock.lang.Specification
  * This class will handle tests for Parser, Typechecker and Executor all together.
  */
 class FullTests extends Specification {
+
+    def 'test that list of TestClass returns correct object'() {
+        given:
+        def code = 'List<it.fulminazzo.mojito.TestClass> list = new LinkedList<>();' +
+                'list.add(new it.fulminazzo.mojito.TestClass());' +
+                'list.add(new it.fulminazzo.mojito.TestClass(1, true));' +
+                'return list.get(0).publicStaticMethod() + list.get(1).publicField;'
+
+        and:
+        Runner runner = Mojito.newRunner()
+
+        when:
+        runner.run(code)
+
+        then:
+        runner.latestResult()
+                .map { (Value) it}
+                .map { it.value }
+                .get() == 2.0
+    }
 
     def 'test that list of numbers can support both doubles and numbers'() {
         given:
