7ffbbbafe7db587481189474b1c2e612c7580315
user <fulminazzo21@gmail.com>
2025-05-10 22:12:12 +0200

    Added tests for SUPER

--- src/test/groovy/it/fulminazzo/mojito/parser/JavaParserTest.groovy
+++ src/test/groovy/it/fulminazzo/mojito/parser/JavaParserTest.groovy
@@ -1019,7 +1019,7 @@ class JavaParserTest extends Specification {
 
         where:
         code                                              | expected
-        'List<?>'                                         | new GenericsLiteral('List', [new EmptyLiteral()])
+        'List<? super String>'                            | new GenericsLiteral('List', [new WildcardSuper(Literal.of('String'))])
         'List<String>'                                    | new GenericsLiteral('List', [Literal.of('String')])
         'Map<String, Integer>'                            | new GenericsLiteral('Map', [
                 Literal.of('String'), Literal.of('Integer')
