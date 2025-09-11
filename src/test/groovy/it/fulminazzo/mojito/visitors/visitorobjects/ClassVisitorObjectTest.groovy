4ae4f864f741f2eebf7e8d768a65ff4310c58b62
user <fulminazzo21@gmail.com>
2025-09-11 16:40:35 +0200

    Added tests for isFunctionalInterface

--- src/test/groovy/it/fulminazzo/mojito/visitors/visitorobjects/ClassVisitorObjectTest.groovy
+++ src/test/groovy/it/fulminazzo/mojito/visitors/visitorobjects/ClassVisitorObjectTest.groovy
@@ -3,7 +3,25 @@ package it.fulminazzo.mojito.visitors.visitorobjects
 import it.fulminazzo.mojito.handler.elements.ClassElement
 import spock.lang.Specification
 
+import java.util.function.Consumer
+import java.util.function.Function
+
 class ClassVisitorObjectTest extends Specification {
+
+    def 'test isFunctionalInterface should return #expected for #clazz'() {
+        when:
+        def element = ClassElement.of(clazz)
+
+        then:
+        element.functionalInterface == expected
+
+        where:
+        clazz    || expected
+        Runnable || true
+        Consumer || true
+        Function || true
+        Object   || false
+    }
 
     def 'test class visitor object should only be compatible with a visitor object instance'() {
         expect:
