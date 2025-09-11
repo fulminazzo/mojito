0211a83f5163f7a072dc9228e551a73afd0d0c22
user <fulminazzo21@gmail.com>
2025-09-11 17:19:11 +0200

    Updated AbstractLambdaType to store lambda parameters names

--- src/main/java/it/fulminazzo/mojito/typechecker/types/AbstractLambdaType.java
+++ src/main/java/it/fulminazzo/mojito/typechecker/types/AbstractLambdaType.java
@@ -1,8 +1,11 @@
 package it.fulminazzo.mojito.typechecker.types;
 
 import it.fulminazzo.mojito.parser.node.Node;
+import it.fulminazzo.mojito.typechecker.types.variables.TypeLiteralVariableContainer;
 import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 /**
  * The following class represents a partially parsed {@link it.fulminazzo.mojito.parser.node.Lambda}
@@ -11,18 +14,28 @@ import org.jetbrains.annotations.NotNull;
  */
 @Getter
 public final class AbstractLambdaType implements Type {
-    private final int parametersCount;
+    private final @NotNull List<TypeLiteralVariableContainer> parametersNames;
     private final @NotNull Node code;
 
     /**
      * Instantiates a new Abstract lambda type.
      *
-     * @param parametersCount the parameters count
+     * @param parametersNames the parameter names
      * @param code            the code
      */
-    public AbstractLambdaType(int parametersCount, final @NotNull Node code) {
-        this.parametersCount = parametersCount;
+    public AbstractLambdaType(final @NotNull List<TypeLiteralVariableContainer> parametersNames,
+                              final @NotNull Node code) {
+        this.parametersNames = parametersNames;
         this.code = code;
+    }
+
+    /**
+     * Gets parameters count.
+     *
+     * @return the parameters count
+     */
+    public int getParametersCount() {
+        return this.parametersNames.size();
     }
 
     @Override
